5e7877a7ce6fe69f7da39a16da6b848bf65235e8
15735394021 <liquan1998@qq.com>
2020-04-06 22:52:06 +0800

    Revert "2020-4-6"
    This reverts commit 0428dc85

--- temp/quick-scripts/dst/__qc_index__.js
+++ temp/quick-scripts/dst/__qc_index__.js
@@ -15,8 +15,6 @@ require('./assets/script/BlackMap');
 require('./assets/script/game');
 require('./assets/script/ground');
 require('./assets/script/ground2');
-require('./assets/script/ground3');
-require('./assets/script/ground3Init');
 require('./assets/script/groundInit');
 require('./assets/script/hero_go');
 
--- temp/quick-scripts/src/assets/script/groundInit.js
+++ temp/quick-scripts/src/assets/script/groundInit.js
@@ -8,49 +8,25 @@ cc.Class({
   "extends": cc.Component,
   properties: {
     hit_audio: {
-      //击打时的音效
       type: cc.AudioClip,
-      "default": null
-    },
-    bg_audio: {
-      //背景音乐
-      type: cc.AudioClip,
-      "default": null
-    },
-    ready_play: {
-      //游戏加载中的进度条
-      type: cc.Node,
-      "default": null
-    },
-    ready_progress: {
-      //游戏加载中的当前进度label
-      type: cc.Label,
-      "default": null
-    },
-    flameParticle: {
-      //火焰粒子，用在村庄门口的开门关门效果
-      type: cc.ParticleSystem,
       "default": null
     }
   },
   onLoad: function onLoad() {
-    var p = cc.director.getPhysicsManager();
-    p.enabled = true;
-    p.gravity = cc.v2(0, 0); // p.debugDrawFlags = true;  //显示出来碰撞边框，为了方便演示
-
-    cc.director.getCollisionManager().enabled = true; //检测碰撞
-    // cc.director.getCollisionManager().enabledDebugDraw = true;//碰撞检测的边框显示
-
+    // cc.director.getCollisionManager().enabled = true;
+    // cc.director.getCollisionManager().enabledDebugDraw = true;
+    // cc.director.getPhysicsManager().enable = true;
     this.groundJs = this.node.getComponent("ground2");
 
     var _this = this;
 
     _this.groundJs.Init = function () {
-      _this.groundJs.mapCameraNode = hero_go.mycamrea;
-      hero_go.node.parent = _this.groundJs.getLayerNodeFun("map"); // console.log(_this.groundJs.getLayerNodeFun("map").Rect1);
+      console.log("初始化");
+      console.log(_this.groundJs.getLayerNodeFun("map").Rect1);
     };
 
     _this.groundJs.onLoadSpriteParent = function (n, LayerName, bo) {//onLoadSpriteParent(父节点：Node，图层名：String，是否第一次加载：bool)
+      // console.log(_this.groundJs.getLayerNodeFun("map").getComponent(cc.BoxCollider));
     };
 
     _this.groundJs.onLoadYMovieClip = function (ymc, layerName, bo) {
@@ -63,78 +39,23 @@ cc.Class({
     };
 
     this.groundJs.onLoadSprite = function (node, name) {
-      //加载图块时调用  onLoadSprite(图块：Node，图层名：String)
+      //加载图块  onLoadSprite(图块：Node，图层名：String)
       if (name == "map") {
         var tag0 = node.getComponent(cc.PolygonCollider);
 
         if (tag0 != null && tag0.tag == 0) {
           var body = node.addComponent(cc.RigidBody);
           body.type = cc.RigidBodyType.Static;
-          var ps = tag0.points;
           var collider = node.addComponent(cc.PhysicsPolygonCollider);
-
-          for (var i = 0; i < ps.length; i++) {
-            if (i > 3) {
-              collider.points.push({
-                x: ps[i].x,
-                y: ps[i].y
-              });
-            } else {
-              collider.points[i].x = ps[i].x;
-              collider.points[i].y = ps[i].y;
-            }
-          }
-
+          collider.offset = tag0.offset;
+          collider.Points = tag0.points;
           collider.apply();
         }
       }
     };
-
-    this.groundJs.killSprite = function (node, name) {
-      //每次图块从舞台上清除时调用    killSprite(图块：Node，图层名：String)
-      if (name == "map") {
-        var tag0 = node.getComponent(cc.PolygonCollider);
-
-        if (tag0 != null && tag0.tag == 0) {
-          var body = node.getComponent(cc.RigidBody);
-          body.destroy();
-        }
-      }
-    };
-
-    this.groundJs.Loading = function (Loaded, Total) {
-      //Loading(已加载的贴图数：Number，总贴图数：Number)
-      var progress = Loaded / Total;
-
-      if (_this.ready_play != null) {
-        _this.ready_progressNum = progress;
-      }
-
-      if (_this.ready_progress != null) {
-        _this.ready_progress.string = "地图正在创建：" + progress * 100 + "%";
-      }
-    };
   },
-  start: function start() {},
-  closeDoor: function closeDoor(point1, point2) {//关门时的点起火焰粒子
-  },
-  update: function update(dt) {
-    if (this.ready_play != null) {
-      var progress = this.ready_play.getComponent(cc.ProgressBar).progress;
+  start: function start() {} // update (dt) {},
 
-      if (progress <= this.ready_progressNum) {
-        progress = progress + 0.01;
-        this.ready_play.getComponent(cc.ProgressBar).progress = progress;
-
-        if (this.ready_play.getComponent(cc.ProgressBar).progress >= 1 && this.ready_play.getComponent(cc.ProgressBar).progress <= 1.02) {
-          this.ready_play.active = false;
-          this.ready_progress.active = false; // this.node.getChildByName("ready_play").destroy();
-
-          cc.audioEngine.play(this.bg_audio, true, 0.3); // this.node.getChildByName("hero").active = true;
-        }
-      }
-    }
-  }
 });
 
 cc._RF.pop();
